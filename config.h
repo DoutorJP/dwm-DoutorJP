ccf95fc6977b9e6fbd61b3b6b04deefc0b0217b8
João Pedro <jpmv14@gmail.com>
2021-12-20 09:49:24 -0300

    New changes/Gentoo theme

--- config.h
+++ config.h
@@ -1,18 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "azukifontB:size=10" };
 static const char dmenufont[]       = "azukifontB:size=10";
-static const char col_gray1[]       = "#000000";
+static const char col_gray1[]       = "#54487a";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#770000";
+static const char col_gray4[]       = "#54487a";
+static const char col_cyan[]        = "#a1a1a1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { " 1 ", " 2 ", " 3 ", " 4 ", " 5 " };
+static const char *tags[] = { " www ", " >_ ", " - ", " = ", " ≡ " };
 
 static const Rule rules[] = {
 	/* xprop(1):
